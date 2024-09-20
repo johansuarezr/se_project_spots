ddc6274fcb1272753d583c0e743a8e396b92a42c
johansuarezr <johansuarez@Johans-MacBook-Air.local>
2024-09-20 11:25:53 -0400

    new changes

--- scripts/index.js
+++ scripts/index.js
@@ -60,11 +60,11 @@ function getCardElement(data) {
 function openModal() {
   editModalNameInput.value = profileName.textContent;
   editModalDescriptionInput.value = profileDescription.textContent;
-  profileEditModal.classList.add("modal__opened");
+  profileEditModal.classList.add("modal_opened");
 }
 
 function closeModal() {
-  profileEditModal.classList.remove("modal__opened");
+  profileEditModal.classList.remove("modal_opened");
 }
 
 function handleProfileFormSubmit(evt) {
